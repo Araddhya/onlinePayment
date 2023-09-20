3c451795c697ec49db0a8098c4b9478112d8ebf7
Araddhya <1941012880.araddhya@gmail.com>
2023-09-20 14:40:14 +0530

    commit

---
+++ Online Payment System/src/upi_payment/Paypal.java
@@ -0,0 +1,14 @@
+package upi_payment;
+
+public class Paypal {
+	int PaYpal2() {
+		System.out.println("helloooo");
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}   
+
+}
---
+++ Online Payment System/src/upi_payment/UPI_Payment.java
@@ -0,0 +1,14 @@
+package upi_payment;
+
+public class UPI_Payment {
+	int PaYpal1() {
+		System.out.println("hiiiiiiiii");
+		return 0;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+	}
+
+}
